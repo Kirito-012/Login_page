aee283c952630cae3dd2ad861d2ddf0f049ff38c
Akshat Kumar <123500480+Kirito-012@users.noreply.github.com>
2024-11-27 15:03:12 +0530

    login_commit

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -23,7 +23,7 @@ const Login = () => {
 		if (!email || !password) return handleError('All input fields are required')
 
 		try {
-			const url = 'http://localhost:4000/auth/login'
+			const url = 'https://login-page-api-peach.vercel.app/auth/login'
 			const response = await fetch(url, {
 				method: 'POST',
 				headers: {
